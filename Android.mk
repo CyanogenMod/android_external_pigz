505a50dabeae5b87c2aa471a02b8a0178e92edb4
Steve Kondik <steve@cyngn.com>
2015-11-10 17:37:46 +0100

    pigz: Update links and makefile for new version
    Change-Id: I4caf6274868bf6bb85feed94def61c939160d90e

--- Android.mk
+++ Android.mk
@@ -1,59 +1,41 @@
 LOCAL_PATH := $(call my-dir)
 
-pigz_src_files := pigz.c yarn.c
-zopfli_src_files := zopfli/blocksplitter.c \
-					zopfli/cache.c \
-					zopfli/deflate.c \
-					zopfli/tree.c \
-					zopfli/lz77.c \
-					zopfli/hash.c \
-					zopfli/util.c \
-					zopfli/squeeze.c \
-					zopfli/katajainen.c
+pigz_src_files := pigz.c try.c yarn.c
+zopfli_src_files := zopfli/src/zopfli/blocksplitter.c \
+					zopfli/src/zopfli/cache.c \
+					zopfli/src/zopfli/deflate.c \
+					zopfli/src/zopfli/tree.c \
+					zopfli/src/zopfli/lz77.c \
+					zopfli/src/zopfli/hash.c \
+					zopfli/src/zopfli/util.c \
+					zopfli/src/zopfli/squeeze.c \
+					zopfli/src/zopfli/katajainen.c
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libzopfli
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(zopfli_src_files)
-LOCAL_C_INCLUDES := $(LOCAL_PATH) external/zlib external/pigz/zopfli
+LOCAL_C_INCLUDES := $(LOCAL_PATH) external/zlib external/pigz/zopfli/src/zopfli
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libpigz
+LOCAL_MODULE := libminipigz_static
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(pigz_src_files)
 LOCAL_C_INCLUDES := $(LOCAL_PATH) external/zlib external/pigz/zopfli
-include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libminipigz
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(pigz_src_files)
-LOCAL_C_INCLUDES := $(LOCAL_PATH) external/zlib external/pigz/zopfli
-LOCAL_CFLAGS := -DWITHOUT_ZOPFLI
+LOCAL_CFLAGS := -Dmain=pigz_main
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := pigz
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := main.c
-LOCAL_SHARED_LIBRARIES := libz libc
-LOCAL_STATIC_LIBRARIES := libpigz libzopfli
+LOCAL_SRC_FILES := $(pigz_src_files)
+LOCAL_STATIC_LIBRARIES := libzopfli
+LOCAL_SHARED_LIBRARIES := libz
+LOCAL_C_INCLUDES := $(LOCAL_PATH) external/zlib external/pigz/zopfli/src/zopfli
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+
+PIGZ_TOOLS := unpigz gzip gunzip
+LOCAL_POST_INSTALL_CMD := $(hide) $(foreach t,$(ALL_TOOLS),ln -sf pigz $(TARGET_OUT)/xbin/$(t);)
+
 include $(BUILD_EXECUTABLE)
-
-PIGZ_TOOLS := unpigz
-SYMLINKS := $(addprefix $(TARGET_OUT_OPTIONAL_EXECUTABLES)/,$(PIGZ_TOOLS))
-$(SYMLINKS): PIGZ_BINARY := $(LOCAL_MODULE)
-$(SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Symlink: $@ -> $(PIGZ_BINARY)"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf $(PIGZ_BINARY) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
-
-# We need this so that the installed files could be picked up based on the
-# local module name
-ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
-    $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
